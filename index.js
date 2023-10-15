395d9716a7adee29b44dadd1617822b283f16d7c
Yan <yandamascoprofissional@gmail.com>
2023-10-15 18:57:52 -0300

    Feat: Responsividade atualizada.

---
+++ index.js
@@ -0,0 +1,15 @@
+let header = document.getElementById('header');
+let navigationHeader = document.getElementById('navigation_header');
+let content = document.getElementById('content');
+let showSidebar = false;
+
+function toggleSidebar() {
+  showSidebar =! showSidebar;
+  if (showSidebar) {
+    navigationHeader.style.marginLeft = '0vw';
+    navigationHeader.style.animationName = 'showSidebar'
+  } else {
+    navigationHeader.style.marginLeft = '-100vw';
+    navigationHeader.style.animationName = ''
+  }
+}
